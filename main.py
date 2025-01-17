422974c3a22f85f9984ed9c7b198b948ca3b9de9
Ryan <Ryantehseen@gmail.com>
2025-01-17 02:18:51 -0500

    added Collisions/Tiles/Physics

--- main.py
+++ main.py
@@ -1,72 +1,135 @@
-import pygame, sys  # Import the pygame and sys modules
+import pygame, sys # import pygame and sys
 
-clock = pygame.time.Clock()  
+clock = pygame.time.Clock() # set up the clock
 
-from pygame.locals import *  
-pygame.init()  #initialize all imported pygame modules
+from pygame.locals import * # import pygame modules
+pygame.init() # initiate pygame
 
-pygame.display.set_caption('Wealth of fields')  #set the title of the window
+pygame.display.set_caption('Pygame Window') # set the window name
 
-WINDOW_SIZE = (400, 400)  #size of the window  
+WINDOW_SIZE = (600,400) # set up window size
 
-player_image = pygame.image.load('Player_standing.png') 
+screen = pygame.display.set_mode(WINDOW_SIZE,0,32) # initiate screen
 
-screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)  #set up the display window
+display = pygame.Surface((300, 200))
+
+
+player_image = pygame.image.load('Player_standing-1.png (3).png').convert()
+player_image.set_colorkey((255, 255, 255))
+
+grass_image = pygame.image.load('gras version 1-1.png (1).png')
+TILE_SIZE = grass_image.get_width()
+
+dirt_image = pygame.image.load('dirt_texture versio1-1.png (1).png')
+### tile map
+game_map = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','2','2','2','2','2','0','0','0','0','0','0','0'],
+            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
+            ['2','2','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','2','2'],
+            ['1','1','2','2','2','2','2','2','2','2','2','2','2','2','2','2','2','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
+            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1']]
+
+def collision_test(rect, tiles):
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
+### 
+def move(rect, movement, tiles):
+    collision_types = {'top': False, 'bottom': False, 'right': False, 'left': False}
+    rect.x += movement[0]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[0] > 0:
+            rect.right = tile.left
+            collision_types['right'] = True
+        elif movement[0] < 0:
+            rect.left = tile.right
+            collision_types['left'] = True
+    rect.y += movement[1]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[1] > 0:
+            rect.bottom = tile.top
+            collision_types['bottom'] = True
+        elif movement[1] < 0:
+            rect.top = tile.bottom
+            collision_types['top'] = True
+    return rect, collision_types
 
 moving_right = False
 moving_left = False
 
-player_location = [50,50]
-Player_y_momentum = 0
+player_y_momentum = 0
+air_timer = 0
 
-player_rect = pygame.Rect(player_location[0],player_location[1],player_image.get_width(),player_image.get_height())
-test_rect = (100,100,100,50)
+player_rect = pygame.Rect(50, 50, player_image.get_width(), player_image.get_height())
+test_rect = pygame.Rect(100,100,100,50)
 
-while True:  ##start the main game loop
-    screen.fill((146,244,255))
-    screen.blit(player_image,player_location)
+while True: # game loop
+    display.fill((146,244,255))
 
+    tile_rects = []
+    y = 0
+    for row in game_map:
+        x = 0
+        for tile in row:
+            if tile == '1':
+                display.blit(dirt_image, (x * TILE_SIZE, y * TILE_SIZE))
+            if tile == '2':
+                display.blit(grass_image, (x * TILE_SIZE, y * TILE_SIZE))
+            if tile != '0':
+                tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            x += 1
+        y += 1
+###  velecity
+    player_movement = [0, 0]
+    if moving_right:
+        player_movement[0] += 2
+    if moving_left:
+        player_movement[0] -= 2
+    player_movement[1] += player_y_momentum
+    player_y_momentum += 0.2
+    if player_y_momentum > 3:
+        player_y_momentum = 3
 
-    #brackets call xyval change
-    if player_location[1] > WINDOW_SIZE[1]-player_image.get_height():
-        Player_y_momentum = -Player_y_momentum
+    player_rect, collisions = move(player_rect, player_movement, tile_rects)
+
+    if collisions['bottom']:
+        player_y_momentum = 0
+        air_timer = 0
     else:
-        Player_y_momentum += 0.2
-    player_location[1] += Player_y_momentum
-
-    #brackets call xval change
-    if moving_right == True:
-        player_location[0] += 4
-    if moving_left == True:
-        player_location[0] -= 4
+        air_timer += 1
     
-    player_rect.x = player_location[0]
-    player_rect.y = player_location[0]
+    display.blit(player_image, (player_rect.x, player_rect.y))
 
-    if player_rect.colliderect(test_rect):
-        pygame.draw.rect(screen,(255,0,0),test_rect)
-    else:
-        pygame.draw.rect(screen,(0,0,0),test_rect)
-
-    ##process events in the event for any usser
-    for event in pygame.event.get():
-
-         #exit program func  
-        if event.type == QUIT:  #quit event is triggered
-            pygame.quit()       #uninitialize all pygame modules
-            sys.exit()          #exit the program
+    for event in pygame.event.get(): # event loop
+        if event.type == QUIT: # check for window quit
+            pygame.quit() # stop pygame
+            sys.exit() # stop script
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 moving_right = True
             if event.key == K_LEFT:
                 moving_left = True
+            if event.key == K_UP:
+                if air_timer < 6:
+                    player_y_momentum = -5
         if event.type == KEYUP:
             if event.key == K_RIGHT:
                 moving_right = False
             if event.key == K_LEFT:
                 moving_left = False
 
-
-
-    pygame.display.update()  #updates any images or updates applied
-    clock.tick(60)           #frame rate to 60 frames per second
+    surf = pygame.transform.scale(display, WINDOW_SIZE)
+    screen.blit(surf, (0, 0))
+    pygame.display.update() # update display
+    clock.tick(60) # maintain 60 fps
